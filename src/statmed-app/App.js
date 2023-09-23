630a47a32a1ca31567cee971507da130b18a06bf
Vinicius Soares <1165546@sga.pucminas.br>
2023-09-23 13:32:54 -0300

    Estrutura Navigator

--- src/statmed-app/App.js
+++ src/statmed-app/App.js
@@ -1,18 +1,45 @@
 import * as React from 'react';
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { AppRegistry } from 'react-native';
 import { PaperProvider } from 'react-native-paper';
 import { name as appName } from './app.json';
 import { theme } from './src/styles/theme/v1/theme';
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialBottomTabNavigator } from "react-native-paper/react-navigation";
+
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import Home from './screens/Home';
+import HospitalHistory from './screens/HospitalHistory';
+
 export default function App() {
+  const Tab = createMaterialBottomTabNavigator();
+
   return (
     <PaperProvider theme={theme}>
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen
+            name="Home"
+            component={Home}
+            options={{
+              tabBarIcon: ({ color }) => (
+                <MaterialCommunityIcons name="home" color={color} size={26} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="HospitalHistory"
+            component={HospitalHistory}
+            options={{
+              tabBarIcon: ({ color }) => (
+                <MaterialCommunityIcons name="cog" color={color} size={26} />
+              ),
+            }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
     </PaperProvider>
   );
 }
---
+++ src/statmed-app/screens/HospitalHistory/index.jsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { Text } from 'react-native-paper';
+import { View } from 'react-native';
+
+export default function HospitalHistory() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>HospitalHistory</Text>
+    </View>
+  );
+}
