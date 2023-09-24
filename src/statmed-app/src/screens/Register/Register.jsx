84c19a5af7ac230e4b69e579e2e4458d3b95a4ba
Vinicius Soares <1165546@sga.pucminas.br>
2023-09-23 21:54:51 -0300

    Login Screen App

---
+++ src/statmed-app/src/screens/Register/Register.jsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import { Text } from 'react-native-paper';
+import { View } from 'react-native';
+import { Image } from 'expo-image';
+import styles from './styles';
+import StatmedVitaLogo from '../../../assets/statmedvita-logo.png'
+
+const blurhash = '|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[';
+
+export default function Register() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Register</Text>
+      <Image
+        style={styles.image}
+        source={StatmedVitaLogo}
+        placeholder={blurhash}
+        contentFit="contain"
+        transition={1000}
+      />
+    </View>
+  );
+}
